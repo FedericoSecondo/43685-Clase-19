a7ae9bd3d2a22adb788cc5cdf51eb34c6b73e249
FedericoSecondo <fsecondo@gmail.com>
2023-07-19 09:08:07 -0300

    Comision 43865/ Clase 19

---
+++ aplicacion/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from .views import *
+urlpatterns = [
+    path('', index, name="inicio" ),
+    path('profesores/', profesores, name="profesores"),
+    path('estudiantes/', estudiantes, name="estudiantes"),
+    path('cursos/', cursos, name="cursos"),
+    path('entregables/', entregables, name="entregables"),
+
+]
